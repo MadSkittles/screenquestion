45e87a1354c655d0b99f3a48de27824ce5cdd881
李偘文 <leekanwen@gmail.com>
2018-09-20 14:03:44 +0800

    Update merge_test.py

--- merge_test.py
+++ merge_test.py
@@ -44,20 +44,24 @@ def read_testcases(testcase_id):
     :return: a tuple with three elements, two linked list as input of merge and a linked list as the answer for checking 
     """
     testcases_dir = os.path.join(os.path.dirname(__file__), "testcases")
-    testcase_file=os.path.join(testcases_dir, "{}.txt".format(testcase_id))
+    testcase_file = os.path.join(testcases_dir, "{}.txt".format(testcase_id))
 
     with open(testcase_file, "r") as f:
         input_list1, input_list2, input_list3 = (
-                f.readline().strip().split(" "),
-                f.readline().strip().split(" "),
-                f.readline().strip().split(" "),
-            )
+            f.readline().strip().split(" "),
+            f.readline().strip().split(" "),
+            f.readline().strip().split(" "),
+        )
         list1, list2, answer = (
-                [int(n) for n in input_list1 if n],
-                [int(n) for n in input_list2 if n],
-                [int(n) for n in input_list3 if n],
-            )
-    return (list_to_linked_list(list1), list_to_linked_list(list2), list_to_linked_list(answer))
+            [int(n) for n in input_list1 if n],
+            [int(n) for n in input_list2 if n],
+            [int(n) for n in input_list3 if n],
+        )
+    return (
+        list_to_linked_list(list1),
+        list_to_linked_list(list2),
+        list_to_linked_list(answer),
+    )
 
 
 class MergeFuncTestCase(unittest.TestCase):
@@ -122,7 +126,6 @@ class MergeFuncTestCase(unittest.TestCase):
         list1, list2, expect_answer = read_testcases(6)
         answer = merge(list1, list2)
         self.assertTrue(check_answer(answer, expect_answer))
-
 
 
 if __name__ == "__main__":
